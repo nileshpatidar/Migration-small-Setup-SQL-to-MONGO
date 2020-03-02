b2e45471fe89f0ab61e20d4081ef40059102c366
NILESH PATIDAR <35222555+nileshpatidar@users.noreply.github.com>
2020-03-02 17:38:37 +0530

    Update sqlconnection.js

--- db/sqlconnection.js
+++ db/sqlconnection.js
@@ -2,19 +2,14 @@
 var mysql = require('mysql');
 
 var MysqlCon = mysql.createConnection({
-//    host:'hoookedup-development.c6kl1j7ko5en.us-east-1.rds.amazonaws.com',
-//    user:'Administrator',
-//    password:'4Vf^A+k8KcN?h-]v',
-//    database:'hookedup_production',
-//    charset:'utf8mb4',
-//    debug    :  false
+//    host: 
+//    user: 
+//    password: 
+//    database
+//    charset:
+//    debug    :
 
-    host:'hoookedup-production.c6kl1j7ko5en.us-east-1.rds.amazonaws.com',
-    user:'Administrator',
-    password:'4Vf^A+k8KcN?h-]v',
-    database:'hookedup_production',
-    charset:'utf8mb4',
-    debug    :  false
+    
    });
    
    MysqlCon.connect(function(err) {
